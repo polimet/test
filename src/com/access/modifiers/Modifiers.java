7b486888d2e4e4e13ef6808fa3d6a9b51ed90234
polimet <pnbabu@gmail.com>
2022-07-21 13:13:09 +0530

    new branch chagnes

--- src/com/access/modifiers/Modifiers.java
+++ src/com/access/modifiers/Modifiers.java
@@ -2,6 +2,7 @@ package com.access.modifiers;
 
 class Modifiers {
 	  public static void main(String[] args) {
+		  
 	    Student myObj = new Student(); 
 	    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
 	    System.out.println("Email: " + myObj.email);
